0be1ba89ad3409df93a4d589a4b8f77f3a22e477
Dorian Mutti <cloudyavi3@gmail.com>
2024-04-20 11:25:13 -0400

    update

--- src/client.java
+++ src/client.java
@@ -1,4 +1,4 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) {
@@ -6,12 +6,12 @@ public class client {
 
         String selection = "";
         int power;
-        int heartRate;
+        //int heartRate;
         int FTP;
-        int FTHR;
-        float unit = 0.95;
+        //int FTHR;
+        double conversion = 0.95;
 
-        System.out.println("Would you like to calcuate FTP or FTHR?: ");
+        System.out.println("Would you like to calculate FTP or FTHR?: ");
         selection = scanner.nextLine();
 
         if (selection.equalsIgnoreCase("ftp") || selection.equalsIgnoreCase("functional threshold power") 
@@ -28,7 +28,8 @@ public class client {
                 }
                 
             }
-            FTP = power * 0.95;
+            FTP = (int) (power * conversion);
+            System.out.println("Your FTP is " + FTP);
         }
     }
 }
