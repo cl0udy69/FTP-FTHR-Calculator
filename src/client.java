7892f3469b5332d6fd463161c9961935521b0c2e
Dorian Mutti <cloudyavi3@gmail.com>
2024-04-23 08:27:21 -0400

    update

--- src/client.java
+++ src/client.java
@@ -5,31 +5,28 @@ public class client {
         Scanner scanner = new Scanner(System.in);
 
         String selection = "";
-        int power;
-        //int heartRate;
+        int power = 0; // Initialize power with a default value
         int FTP;
-        //int FTHR;
         double conversion = 0.95;
 
         System.out.println("Would you like to calculate FTP or FTHR?: ");
         selection = scanner.nextLine();
 
-        if (selection.equalsIgnoreCase("ftp") || selection.equalsIgnoreCase("functional threshold power") 
-        || selection.equalsIgnoreCase("threshold power") || selection.equalsIgnoreCase("power")) {
-            while (true) {
-                System.out.println("Enter your current 20 minute power: ");
-                power = scanner.nextInt();
-
-                try {
-                    power = Integer.parseInt(scanner.nextLine());
-                    break;
-                } catch (NumberFormatException nfe) {
-                    System.out.println(power + " is not a valid input");
-                }
-                
+        if (selection.equalsIgnoreCase("ftp") || selection.equalsIgnoreCase("functional threshold power")
+                || selection.equalsIgnoreCase("threshold power") || selection.equalsIgnoreCase("power")) {
+            System.out.println("Enter your best 20 minute power: ");
+            try {
+                power = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid input");
             }
+
             FTP = (int) (power * conversion);
             System.out.println("Your FTP is " + FTP);
-        }
+            while (true) {
+                selection = scanner.nextLine();
+            }
+        } 
+        scanner.close();
     }
 }
