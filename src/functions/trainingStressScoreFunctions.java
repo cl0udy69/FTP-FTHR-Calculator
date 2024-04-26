71b8525d0d2ab0da6f3047bc3a2721b567c04a5a
Dorian Mutti <cloudyavi3@gmail.com>
2024-04-26 15:04:36 -0400

    finished TSS calculation function

--- src/functions/trainingStressScoreFunctions.java
+++ src/functions/trainingStressScoreFunctions.java
@@ -1,4 +1,5 @@
 package functions;
+
 import java.util.Scanner;
 
 public class trainingStressScoreFunctions {
@@ -11,7 +12,7 @@ public class trainingStressScoreFunctions {
         String selection = "";
         int duration = 0;
         int normalizedPower = 0;
-        int intensityFactor = 0;
+        float intensityFactor = 0;
         int functionalThresholdPower = 0;
         int trainingStressScore = 0;
 
@@ -30,7 +31,7 @@ public class trainingStressScoreFunctions {
             try {
                 normalizedPower = Integer.parseInt(scanner.nextLine());
                 break;
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter a valid number.");
             }
         }
@@ -38,9 +39,9 @@ public class trainingStressScoreFunctions {
         System.out.println("Enter your intensity factor (round decimal up or down to nearest whole number): ");
         while (true) {
             try {
-                intensityFactor = Integer.parseInt(scanner.nextLine());
+                intensityFactor = Float.parseFloat(scanner.nextLine());
                 break;
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter a valid number.");
             }
         }
@@ -50,12 +51,13 @@ public class trainingStressScoreFunctions {
             try {
                 functionalThresholdPower = Integer.parseInt(scanner.nextLine());
                 break;
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter a valid number.");
             }
         }
 
-        trainingStressScore = ((duration * normalizedPower * intensityFactor) / (functionalThresholdPower * 3600)) * 100;
+        trainingStressScore = (int) (((duration * normalizedPower * intensityFactor)
+                / (functionalThresholdPower * 3600)) * 100);
 
         System.out.println("Your training stress score is: " + trainingStressScore);
         System.out.println("Would you like to do something else?");
@@ -64,20 +66,26 @@ public class trainingStressScoreFunctions {
             if (selection.equalsIgnoreCase("yes") || selection.equalsIgnoreCase("y")) {
                 System.out.println("What would you like to do?");
                 selection = scanner.nextLine();
-                if (selection.equalsIgnoreCase("calculate ftp") || (selection.equalsIgnoreCase("calculate functional threshold power")) ||
-                        (selection.equalsIgnoreCase("ftp")) || (selection.equalsIgnoreCase("functional threshold power"))) {
+                if (selection.equalsIgnoreCase("calculate ftp")
+                        || (selection.equalsIgnoreCase("calculate functional threshold power")) ||
+                        (selection.equalsIgnoreCase("ftp"))
+                        || (selection.equalsIgnoreCase("functional threshold power"))) {
                     powerFunc.calculateFTP();
-                } else if (selection.equalsIgnoreCase("lthr") || selection.equalsIgnoreCase("lactate threshold heart rate")
-                        || selection.equalsIgnoreCase("calculate lthr") || selection.equalsIgnoreCase("calculate lactate threshold heart rate")) {
+                } else if (selection.equalsIgnoreCase("lthr")
+                        || selection.equalsIgnoreCase("lactate threshold heart rate")
+                        || selection.equalsIgnoreCase("calculate lthr")
+                        || selection.equalsIgnoreCase("calculate lactate threshold heart rate")) {
                     heartRateFunc.calculateHeartRate();
                 } else if (selection.equalsIgnoreCase("tss") || selection.equalsIgnoreCase("training stress score") ||
-                        (selection.equalsIgnoreCase("calculate tss")) || (selection.equalsIgnoreCase("calculate training stress score"))) {
+                        (selection.equalsIgnoreCase("calculate tss"))
+                        || (selection.equalsIgnoreCase("calculate training stress score"))) {
                     tssFunc.calculateTrainingStressScore();
                 } else {
                     System.out.println("Thank you for using my program!");
                 }
             }
             scanner.close();
+            break; // added break statement to exit while loop
         }
     }
 }
