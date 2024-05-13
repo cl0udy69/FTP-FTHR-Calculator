97096f9066257a82b2e5090c841a98bafe2467a6
Dorian Mutti <cloudyavi3@gmail.com>
2024-05-13 13:47:02 -0400

    - Create nutrition plan for running - Combined lose/gain weight into one input panel - Combined running/cycling into one input panel

--- src/UI/methods/CreateNutritionPlanDialog.java
+++ src/UI/methods/CreateNutritionPlanDialog.java
@@ -55,8 +55,7 @@ public class CreateNutritionPlanDialog extends JDialog {
         inputPanel.setBorder(new EmptyBorder(30, 30, 30, 30)); // Add empty border for spacing
 
         // Display option dialog to choose objective
-        String[] options = { "Gain Weight", "Lose Weight", "Improve Performance", "Track Nutrition", "Cycling",
-                "Running" };
+        String[] options = { "Gain Weight", "Lose Weight", "Cycling", "Running" };
         int choice = JOptionPane.showOptionDialog(null, "Select One", "Choose Objective",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
         if (choice == JOptionPane.CLOSED_OPTION) {
@@ -68,6 +67,7 @@ public class CreateNutritionPlanDialog extends JDialog {
         if (options[choice].equals("Cycling")) {
             // Display another option dialog to choose cycling type
             String[] cyclingOptions = { "General", "Training Ride" };
+            
             int cyclingChoice = JOptionPane.showOptionDialog(null, "Select One", "Choose Cycling Type",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, cyclingOptions,
                     cyclingOptions[0]);
@@ -80,17 +80,28 @@ public class CreateNutritionPlanDialog extends JDialog {
             } else if (cyclingOptions[cyclingChoice].equals("Training Ride")) {
                 initializeTrainingRideCyclingNutritionInputPanel(inputPanel);
             }
+        
         } else if (cyclingOptions[cyclingChoice].equals("Gain Weight")){
             initializeGainWeightInputPanel(inputPanel);
+        
         } else if (cyclingOptions[cyclingChoice].equals("Lose Weight")) {
-            initializeLoseWeightInputPanel(inputPanel);
-        } else {
-            // Show message for other options (like "Running") that are not yet implemented
-            JOptionPane.showMessageDialog(this, "Coming Soon!", "Feature in Development",
-                    JOptionPane.WARNING_MESSAGE);
-            dispose();
-            return;
-        }
+            initializeGainWeightInputPanell(inputPanel);
+        
+        } if (options[choice.equals("Running")]) { 
+            String[] runningOptions = {"General", "Training Run"};
+            
+            int runningChoice = JOptionPane.showOptionDialog(null, "Select One", "Choose Cycling Type",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, runningOptions,
+                    cyclingOptions[0]);
+
+            if (runningChoice == JOptionPane.CLOSED) {
+                dispose();
+                return;
+            } else if (runningOptions[runningChoice].equals("General")) {
+                initializeGeneralCyclingNutritionInputPanel(inputPanel);
+            } else if (runningOptions[runningChoice].equals("Training Run")) {
+                initializeGeneralCyclingNutritionInputPanel(inputPanel);
+            }
 
         // Add input panel to the dialog
         add(inputPanel, BorderLayout.CENTER);
@@ -331,113 +342,6 @@ public class CreateNutritionPlanDialog extends JDialog {
     }
 
     private void createGainWeightNutritionPlan() {
-        try{
-            int currentWeight = Integer.parseInt(currentWeightField.getText());
-            int goalWeight = Integer.parseInt(goalWeightField.getText());
-            int excerciseDuration = Integer.parseInt(excerciseDurationField.getText());
-            int caloriesBurned = Integer.parseInt(caloriesBurnedField.getText());
-            int carbsBurned = Integer.parseInt(carbsBurnedField.getText());
-            int proteinTarget = Integer.parseInt(proteinTargetField.getText());
-            int carbTarget = Integer.parseInt(carbTargetField.getText());
-            int fatTarget = Integer.parseInt(fatTargetField.getText());
-            int ironTarget = Integer.parseInt(ironTargetField.getText());
-            int sugarTarget = Integer.parseInt(sugarTargetField.getText());
-
-            String date = dateField.getText();
-            String height = heightField.getText();
-
-            StringBuilder planDetails = new StringBuilder();
-            planDetails.append("Nutrition Plan: \n");
-            planDetails.append("Date: ").append(date).append("\n");
-            planDetails.append("Height: ").append(height).append("\n");
-            planDetails.append("Current Weight: ").append(currentWeight).append("\n");
-            planDetails.append("Goal Weight: ").append(goalWeight).append("\n");
-            planDetails.append("Excercise: ").append(excerciseField.getText()).append("\n");
-            planDetails.append("Excercise Duration: ").append(excerciseDuration).append("\n");
-            planDetails.append("Calories Burned: ").append(caloriesBurned).append("\n");
-            planDetails.append("Carbs Burned: ").append(carbsBurned).append("g").append("\n");
-            planDetails.append("Protein Target: ").append(proteinTarget).append("g").append("\n");
-            planDetails.append("Carb Target: ").append(carbTarget).append("g").append("\n");
-            planDetails.append("Fat Target: ").append(fatTarget).append("g").append("\n");
-            planDetails.append("Iron Target: ").append(ironTarget).append("g").append("\n");
-            planDetails.append("Sugar Target: ").append(sugarTarget).append("g").append("\n");
-
-            parentGUI.getOutputTextArea().append(planDetails.toString());
-            JOptionPane.showMessageDialog(parentGUI, "Nutrition plan created successfully!", "Success",
-                    JOptionPane.INFORMATION_MESSAGE);
-            dispose(); // Dispose the dialog after creating the plan
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(parentGUI, "Please enter valid numbers for all fields.", "Error",
-                    JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private void initializeLoseWeightInputPanel(JPanel inputPanel) {
-        inputPanel.removeAll();
-        inputPanel.setLayout(new GridLayout(16, 2, 10, 10));
-
-        JLabel dateLabel = new JLabel("Date: ");
-        dateField = new JTextField();
-        JLabel heightLabel = new JLabel("Height: ");
-        heightField = new JTextField();
-        JLabel currentWeightLabel = new JLabel("Current Weight: ");
-        currentWeightField = new JTextField();
-        JLabel goalWeightLabel = new JLabel("Goal Weight: ");
-        goalWeightField = new JTextField();
-        JLabel excerciseLabel = new JLabel("Excercise: ");
-        excerciseField = new JTextField();
-        JLabel excerciseDurationLabel = new JLabel("Excercise Duration: ");
-        excerciseDurationField = new JTextField(); 
-        JLabel caloriesBurnedlabel = new JLabel("Calories Burned: ");
-        caloriesBurnedField = new JTextField();
-        JLabel carbsBurnedLabel = new JLabel("Carbs Burned: ");
-        carbsBurnedField = new JTextField();
-        JLabel proteinTargetlabel = new JLabel("Target Protein Consumption: ");
-        proteinTargetField = new JTextField();
-        JLabel carbTargetLabel = new JLabel("Target Carb Consumption: ");
-        carbTargetField = new JTextField();
-        JLabel fatTargetLabel = new JLabel("Target Fat Consumption: ");
-        fatTargetField = new JTextField();
-        JLabel ironTargetLabel = new JLabel("Target Iron Consumption: ");
-        ironTargetField = new JTextField();
-        JLabel sugarTargetLabel = new JLabel("Target Sugar Consumption: ");
-        sugarTargetField = new JTextField();
-
-        inputPanel.add(dateLabel);
-        inputPanel.add(dateField);
-        inputPanel.add(currentWeightLabel);
-        inputPanel.add(currentWeightField);
-        inputPanel.add(goalWeightLabel);
-        inputPanel.add(goalWeightField);
-        inputPanel.add(excerciseLabel);
-        inputPanel.add(excerciseField);
-        inputPanel.add(excerciseDurationLabel);
-        inputPanel.add(excerciseDurationField);
-        inputPanel.add(heightLabel);
-        inputPanel.add(heightField);
-        inputPanel.add(caloriesBurnedlabel);
-        inputPanel.add(caloriesBurnedField);
-        inputPanel.add(carbsBurnedLabel);
-        inputPanel.add(carbsBurnedField);
-        inputPanel.add(proteinTargetlabel);
-        inputPanel.add(proteinTargetField);
-        inputPanel.add(carbTargetLabel);
-        inputPanel.add(carbTargetField);
-        inputPanel.add(fatTargetLabel);
-        inputPanel.add(fatTargetField);
-        inputPanel.add(ironTargetLabel);
-        inputPanel.add(ironTargetField);
-        inputPanel.add(sugarTargetLabel);
-        inputPanel.add(sugarTargetField);
-
-        createButton = new JButton("Create");
-        createButton.addActionListener(e -> createNutritionPlan());
-
-        add(inputPanel, BorderLayout.CENTER);
-        add(createButton, BorderLayout.SOUTH);
-    }
-
-    private void createLoseWeightNutritionPlan() {
         try{
             int currentWeight = Integer.parseInt(currentWeightField.getText());
             int goalWeight = Integer.parseInt(goalWeightField.getText());
